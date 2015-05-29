71781c4852faf611b51fc3d8d72928d9a42fb497
Kovalevskiy Vasil <skif24-91@mail.ru>
2015-05-29 13:43:57 +1000

    fix new

--- lib/rmpgw/issue_patch.rb
+++ lib/rmpgw/issue_patch.rb
@@ -5,6 +5,7 @@ module Rmpgw
 
       base.class_eval do
         alias_method_chain :addable_watcher_users, :rmpgw
+        alias_method_chain :watcher_user_ids=, :rmpgw
       end
     end
 
@@ -12,6 +13,15 @@ module Rmpgw
       def addable_watcher_users_with_rmpgw
         users = addable_watcher_users_without_rmpgw
         Group.sorted.to_a + users
+      end
+
+      def watcher_user_ids_with_rmpgw=(user_ids)
+        if user_ids.is_a?(Array)
+          user_ids = user_ids.uniq
+          User.includes(:groups).active.where("groups_users.id in (:user_ids) or #{User.table_name}.id in (:user_ids)", user_ids: user_ids + [0]).uniq.sorted.map(&:id)
+        end
+
+        send :watcher_user_ids_without_rmpgw=, user_ids
       end
     end
   end
